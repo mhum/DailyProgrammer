8fd86e299ee8129fa67d7305c7418eb40c35fd94
Michael <Michael@MichaelHumiston.com>
2016-12-07 14:57:49 -0800

    Bonus 1 for #294

--- 294/RackManagement.java
+++ 294/RackManagement.java
@@ -7,14 +7,18 @@ public class RackManagement {
 		
 		for (char c : wordArray) {
 			int letterIndex = lettersBuilder.indexOf(Character.toString(c));
-			if ( letterIndex > -1) {
+			if (letterIndex > -1) {
 				lettersBuilder.deleteCharAt(letterIndex);
 			} else {
-				return false;
+				int wcIndex = lettersBuilder.indexOf("?");
+				if (wcIndex > -1) {
+					lettersBuilder.deleteCharAt(wcIndex);
+				} else {
+					return false;					
+				}
 			}
 		}
 		
 		return true;
-	}
-	
+	}	
 }
--- 294/RackManagementTest.java
+++ 294/RackManagementTest.java
@@ -11,5 +11,15 @@ public class RackManagementTest {
 		assertEquals(RackManagement.scrabble("orrpgma", "program"), true);
 		assertEquals(RackManagement.scrabble("orppgma", "program"), false);
 	}
+	
+	@Test
+	public void bonus1() {
+		assertEquals(RackManagement.scrabble("pizza??", "pizzazz"), true);	
+		assertEquals(RackManagement.scrabble("pisza??", "pizzaz"), true);	
+		assertEquals(RackManagement.scrabble("pi?za?s", "pizzaz"), true);
+		assertEquals(RackManagement.scrabble("piizza?", "pizzazz"), false);
+		assertEquals(RackManagement.scrabble("a??????", "program"), true);
+		assertEquals(RackManagement.scrabble("b??????", "program"), false);
+	}
 
 }
