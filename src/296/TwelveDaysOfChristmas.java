07d2b03d98b98aae89ce38628c40c61e5c6ef4e3
Michael <Michael@MichaelHumiston.com>
2016-12-21 14:35:08 -0800

    #296 Bonus 1

--- src/296/TwelveDaysOfChristmas.java
+++ src/296/TwelveDaysOfChristmas.java
@@ -3,9 +3,9 @@ public class TwelveDaysOfChristmas {
   public static void main(String[] args) {
     String[] days = { "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth",
         "eleventh", "twelfth" };
-    String[] gifts = { "1 Partridge in a Pear Tree", "2 Turtle Doves", "3 French Hens", "4 Calling Birds",
-        "5 Golden Rings", "6 Geese a Laying", "7 Swans a Swimming", "8 Maids a Milking", "9 Ladies Dancing",
-        "10 Lords a Leaping", "11 Pipers Piping", "12 Drummers Drumming" };
+    String[] gifts = { "a Partridge in a Pear Tree", "two Turtle Doves", "three French Hens", "four Calling Birds",
+        "five Golden Rings", "six Geese a Laying", "seven Swans a Swimming", "eight Maids a Milking", "nine Ladies Dancing",
+        "ten Lords a Leaping", "eleven Pipers Piping", "twelve Drummers Drumming" };
 
     for (int i = 0; i < days.length; i++) {
       System.out.println("On the " + days[i] + " day of Christmas");
